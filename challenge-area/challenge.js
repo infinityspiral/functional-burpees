f28017cb920936b32fa900318a347f5c36f96eb7
CN58090 <NortonChristopherP@johndeere.com>
2017-11-17 10:35:44 -0600

    refactor: reworking to test understanding of promises

--- challenge-area/challenge.js
+++ challenge-area/challenge.js
@@ -1,15 +1,10 @@
-//Good luck! Maybe start by making that fetch request ;)
-const getData = () => {
-  const DATA_URL = 'https://opentdb.com/api.php?amount=10&category=18';
-  return fetch(DATA_URL)
-  .catch(e => console.log(e))
-  .then(res => res.json())
-  .then(data => data.results);
-}
-
-getData()
+const BASE_URL = 'https://opentdb.com/api.php?amount=10&category=18';
+fetch(BASE_URL)
+.catch(e => console.log(e))
+.then(data => data.json())
+.then(data => data.results)
 .then(results => {
-  // task 1
+  console.log('TASK: 1');
   const cleanQuotes = results.map(result => {
     const matcher = new RegExp(/&quot;, '\"'/g);
     const cleanedQuestion = result.question.replace(matcher, '"');
@@ -20,12 +15,10 @@ getData()
     }
   })
   console.log(cleanQuotes);
-
-  // task 2
+  console.log('TASK: 2');
   const easyQuestions = cleanQuotes.filter(result => result.difficulty === 'easy');
   console.log(easyQuestions);
-
-  // task 3
+  console.log('TASK: 3');
   const sortedDifficulty = cleanQuotes.map(result => {
     let difficultyNum = 0;
     if(result.difficulty === 'easy') difficultyNum = 0;
@@ -38,19 +31,16 @@ getData()
   })
   .sort((a,b) => a.difficultyNum > b.difficultyNum);
   console.log(sortedDifficulty);
-
-  // task 4
+  console.log('TASK: 4');
   const questionTotals = {
     easy: sortedDifficulty.filter(result => result.difficultyNum===0).length,
     medium: sortedDifficulty.filter(result => result.difficultyNum===1).length,
     hard: sortedDifficulty.filter(result => result.difficultyNum===2).length
   }
   console.log(questionTotals);
-
-  // task 5
+  console.log('TASK: 5');
   console.log(cleanQuotes.every(result => result.category === 'Science: Computers'))
-
-  // bonus
+  console.log('BONUS');
   const sortedByChoiceTypeMedium = cleanQuotes.filter(result => {
     return result.difficulty === 'medium'
   })
